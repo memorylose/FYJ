75bd7fe2a0764f615c4d7558ba02941be020eaa7
Cass <cass.fan@fabricgroup.com.au>
2015-08-28 16:54:45 +0800

    update

---
+++ Code/FYJ/FYJ/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FYJ.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User register
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        ////POST: User register
+        //[HttpPost]
+        //public ActionResult Register()
+        //{
+        //    return View();
+        //}
+    }
+}
