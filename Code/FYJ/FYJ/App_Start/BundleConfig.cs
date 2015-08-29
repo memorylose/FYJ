a044926a78e5ca434c6fde22bb06a3b456060455
memorylose <lucifering@126.com>
2015-08-29 10:59:08 +0800

    update

---
+++ Code/FYJ/FYJ/App_Start/BundleConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Optimization;
+
+namespace FYJ.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Script/jquery-1.11.3.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                        "~/Script/bootstrap.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/logincss").Include(
+                      "~/Style/bootstrap.min.css",
+                      "~/Style/Login.css"));
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
