c6851af92f74b035f991df741ca2eb5463d84328
Cass <cass.fan@fabricgroup.com.au>
2015-09-01 16:56:50 +0800

    update

---
+++ Code/FYJ/FYJ.BLL/UserRegisterRepository.cs
@@ -0,0 +1,46 @@
+﻿using FYJ.IBLLStrategy;
+using FYJ.Model;
+using Newtonsoft.Json;
+using System;
+
+namespace FYJ.BLL
+{
+    public class UserRegisterRepository : IRegister
+    {
+        private ApplicationContext db = new ApplicationContext();
+
+        /// <summary>
+        /// User regist
+        /// </summary>
+        /// <param name="model"></param>
+        public void Register(RegisterModel model)
+        {
+            User dbUser = new User();
+            dbUser.UserName = model.Email;
+            dbUser.Password = model.Passoword;
+            dbUser.Email = model.Email;
+            dbUser.EmailCode = "";
+            dbUser.EmailConfirm = false;
+            dbUser.IsLock = false;
+            dbUser.IsDelete = false;
+            dbUser.LockDate = null;
+            dbUser.RoleId = 1;//User
+            dbUser.CrDate = DateTime.Now;
+            dbUser.CrUserId = 0;
+
+            db.User.Add(dbUser);
+            db.SaveChanges();
+        }
+
+        /// <summary>
+        /// Get user model from user json
+        /// </summary>
+        /// <param name="registerJson"></param>
+        /// <returns></returns>
+        public RegisterModel GetJsonModel(string registerJson)
+        {
+            //TODO : CHECK JSON FORMAT
+            return JsonConvert.DeserializeObject<RegisterModel>(registerJson);
+        }
+    }
+}
---
+++ Code/FYJ/FYJ.IBLLStrategy/IUserRegister.cs
@@ -0,0 +1,14 @@
+﻿using FYJ.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FYJ.IBLLStrategy
+{
+    public interface IRegister
+    {
+        void Register(RegisterModel model);
+    }
+}
