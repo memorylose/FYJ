ebbfeb26879d9b240d04d01ddf89980d4eb27613
unknown <cass.fan@CassFan.FabricHQ.Local>
2015-09-15 17:57:11 +0800

    update

---
+++ Code/FYJ/FYJ.BLL/UserRole.cs
@@ -0,0 +1,29 @@
+﻿using FYJ.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FYJ.BLL
+{
+    public class UserRole
+    {
+        private ApplicationContext db = new ApplicationContext();
+
+        /// <summary>
+        /// Check user role
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="roleId"></param>
+        /// <returns></returns>
+        public bool CheckUserRole(int userId, int roleId)
+        {
+            User userModel = db.User.Where(c => c.UserId == userId).FirstOrDefault();
+            if (userModel.RoleId != null && userModel.RoleId == roleId)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ Code/FYJ/FYJ/AttributeHanlder/AttributeRepository.cs
@@ -0,0 +1,42 @@
+﻿using FYJ.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FYJ.AttributeHanlder
+{
+    public class RoleAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            UserRole userRole = new UserRole();
+
+            //TODO
+            if (!userRole.CheckUserRole(0, 0))
+            {
+                string urlreffer = filterContext.HttpContext.Request.UrlReferrer == null ? string.Empty
+                                           : filterContext.HttpContext.Request.UrlReferrer.AbsoluteUri;
+                if (string.IsNullOrEmpty(urlreffer))
+                    urlreffer = filterContext.HttpContext.Request.Url == null ? string.Empty : filterContext.HttpContext.Request.Url.AbsoluteUri;
+
+                string locationUrl = string.Empty;
+                locationUrl = TsingDa.Common.WebConfig.GetWebConfig("website_url", "") + "/Home/Login?ReturnUrl=" +
+                                            filterContext.HttpContext.Server.UrlEncode(urlreffer);
+                RedirectResult loginUrl = new RedirectResult(locationUrl);
+                filterContext.Result = loginUrl;
+            }
+            else
+            {
+                //---------【验证用户的控制器的访问权限（利用位运算灵活的解决多角色问题）】---------
+                if (((int)_role & (int)memberLoginRole) <= 0 && ((int)_role != memberLoginRole))
+                {
+                    filterContext.Result = new RedirectResult("/Home/RoleError");
+                }
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
