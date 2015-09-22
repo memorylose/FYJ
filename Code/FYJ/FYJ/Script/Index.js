a6adbae4f7112d73ed87a2f9d399a42e66663ef6
unknown <cass.fan@CassFan.FabricHQ.Local>
2015-09-22 16:49:10 +0800

    update

---
+++ Code/FYJ/FYJ/Script/Index.js
@@ -0,0 +1,18 @@
+﻿function UploadChange(value, files) {
+    var objUrl = getObjectURL(files);
+    console.log("objUrl = " + objUrl);
+    if (objUrl) {
+        $('#imageList').append('<img src="' + objUrl + '" style="width:50px; height:50px;"/>');
+    }
+}
+function getObjectURL(file) {
+    var url = null;
+    if (window.createObjectURL != undefined) { // basic
+        url = window.createObjectURL(file);
+    } else if (window.URL != undefined) { // mozilla(firefox)
+        url = window.URL.createObjectURL(file);
+    } else if (window.webkitURL != undefined) { // webkit or chrome
+        url = window.webkitURL.createObjectURL(file);
+    }
+    return url;
+}
